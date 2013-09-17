ffa5363c0c2121125024ab87986e6601d0291262
Denis Colli Spalenza <deniscsz@gmail.com>
2013-09-17 15:03:38 -0300

    Observers created

---
+++ app/code/community/Xpd/EmailForge/Model/Observer.php
@@ -0,0 +1,13 @@
+<?php
+class Xpd_EmailForge_Model_Observer
+{
+    public function getSubscribeCustomer(Varien_Event_Observer $observer)
+    {
+        
+    }
+    
+    public function getSubscriber(Varien_Event_Observer $observer)
+    {
+        
+    }
+}
